d6a95179499192cf9fa94563f0dad96e4a931dbd
Alisson Nascimento <alissonnascimento321@gmail.com>
2023-04-09 19:48:07 -0300

    Add Quiz

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -1,83 +1,111 @@
-import style from './Chatbox.module.css'
-import { useState } from "react";
+import style from './Chatbox.module.css';
+import { useState } from 'react';
+import { AiOutlineUser } from 'react-icons/ai';
+import {CardResult, TitleResult, ContentResult} from './styles.chatbot';
+import { BsFillSendFill , BsEraserFill } from "react-icons/bs";
 
-const OPENAI_API_KEY = "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";
+const OPENAI_API_KEY = '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';
 
 export function ChatGPT() {
-  const [inputValue, setInputValue] = useState("");
-  const [resultValue, setResultValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
+  const [resultValue, setResultValue] = useState('');
 
   const handleKeyPress = (e) => {
-    if (inputValue && e.key === "Enter") sendQuestion();
+    if (inputValue && e.key === 'Enter') sendQuestion();
   };
 
   const sendQuestion = () => {
-    const sQuestion = inputValue;
-    setInputValue("Carregando...");
-    setResultValue((prevResult) => prevResult + `\n\n\nEu: ${sQuestion}`);
+    const sQuestion =  inputValue;
+    setInputValue('Carregando...');
+    setResultValue((prevResult) => prevResult + `\n 👤 \n ${sQuestion} \n `);
 
-    fetch("https://api.openai.com/v1/completions", {
-      method: "POST",
+    fetch('https://api.openai.com/v1/completions', {
+      method: 'POST',
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + OPENAI_API_KEY,
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + OPENAI_API_KEY,
       },
       body: JSON.stringify({
-        model: "text-davinci-003",
-        prompt: sQuestion,
+        model: 'text-davinci-003',
+        prompt: "Explique de maneira tecnica:" + sQuestion,
         max_tokens: 2048,
         temperature: 0.5,
       }),
     })
       .then((response) => response.json())
       .then((json) => {
-        if (resultValue) setResultValue((prevResult) => prevResult + "\n");
+
+       
+        if (resultValue) setResultValue((prevResult) => prevResult + '\n' );
 
         if (json.error?.message) {
           setResultValue(
             (prevResult) => prevResult + `Error: ${json.error.message}`
           );
         } else if (json.choices?.[0].text) {
-          const text = json.choices[0].text || "Sem resposta";
-          setResultValue((prevResult) => prevResult + `Tutor: ${text}`);
+          const text = json.choices[0].text || 'Sem resposta';
+
+          setResultValue((prevResult) =>  prevResult + `\n 👨‍💻 ${text} \n`);
         }
 
         setResultValue((prevResult) => {
-          const resultTextArea = document.getElementById("result");
+          const resultTextArea = document.getElementById('result');
           resultTextArea.scrollTop = resultTextArea.scrollHeight;
           return prevResult;
         });
       })
-      .catch((error) => console.error("Error:", error))
+      
+      .catch((error) => console.error('Error:', error))
       .finally(() => {
-        setInputValue("");
-        const inputTextArea = document.getElementById("inputQuestion");
+        setInputValue('');
+        const inputTextArea = document.getElementById('inputQuestion');
         inputTextArea.focus();
       });
   };
-  return (
-    <div className="content">
-      <textarea
-        className={style.result}
-        id="result"
-        rows="10"
-        disabled
-        value={resultValue}
-        placeholder="Resposta do Tutor"
-      ></textarea>
 
-      <textarea
-        id="inputQuestion"
-        value={inputValue}
-        placeholder="Inicie o seu aprendizado"
-        onChange={(e) => setInputValue(e.target.value)}
-        onKeyPress={handleKeyPress}
-        disabled={inputValue === "Carregando..."}
-      ></textarea>
+  const handleClear = () => {
+    setResultValue('');
+  };
+  const handleSendQuestion = () => {
+    if (inputValue) sendQuestion();
+  };
+
+  return (
+    <div className={style.content}>
+      <div className={style.textareaWrapper}>
+        
+        <textarea
+          disabled
+          className={style.result}
+          id="result"
+          rows="10"
+          value={resultValue}
+          placeholder="Resposta do Tutor"
+        ></textarea>
+
+        <div className={style.inputWrapper}>
+          <textarea
+            className={style.value}
+            id="inputQuestion"
+            value={inputValue}
+            rows="5"
+            placeholder="Digite a sua pergunta"
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyPress={handleKeyPress}
+            disabled={inputValue === 'Carregando...'}
+          ></textarea>
+
+          <div className={style.inputButton}>
+            <button className={style.send} onClick={handleSendQuestion}>
+              < BsFillSendFill /> 
+            </button>
+            <button className={style.clear} onClick={handleClear}>
+              <strong> < BsEraserFill /></strong>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
-
-
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,39 +1,53 @@
-import { ChatGPT } from "./Chatbox"
-import { AiOutlineCloseCircle } from "react-icons/ai";
-import style from './Home.module.css'
+import { ChatGPT } from "./Chatbox";
+import style from './Home.module.css';
 import React, { useState } from 'react';
+import { BsHouseDoorFill } from "react-icons/bs";
+import { Quiz } from "./Quiz";
+import { BsSlack , BsJoystick } from "react-icons/bs";
 
 
- 
 export function Home() {
   const [showChat, setShowChat] = useState(false);
-
-  const handleClick = () => {
+  const [showQuiz, setShowQuiz] = useState(false);
+ 
+  const handleChatClick = () => {
     setShowChat(true);
   };
 
-  return (
-    <div>
-        {!showChat &&
-      <div className={`${style.inicar} ${style.link}`}>
-        <div>
-          
-        </div>
-          <span className={style.start}> <strong>Guia</strong> </span>
-          <span className={style.tutor}><strong>do</strong> </span>
-          <span className={style.personalizado}> <strong>Saber</strong> </span>
-          <h2 className={style.subtitle}>Descubra o mundo do conhecimento!</h2>
-          <button className={style.journey} onClick={handleClick}><strong> Iniciar Jornada </strong> </button>
-       </div>
-     }
-      
-    
+  const handleQuizClick = () => {
+    setShowQuiz(true);
+  };
 
+  const handleBackClick = () => {
+    setShowChat(false);
+    setShowQuiz(false);
+  };
+
+  return (
     
+    <div>
+      {showChat && <button className={`${style.exit} ${style.icon} `} onClick={handleBackClick}> <BsHouseDoorFill /> </button>}
+      {showQuiz && <button className={`${style.exit} ${style.icon} `} onClick={handleBackClick}> <BsHouseDoorFill /> </button>}
+      
+      {!showChat && !showQuiz &&
+        <div className={`${style.inicar} ${style.link}`}>
+          <span className={style.start}><strong>Guia</strong></span>
+          <span className={style.tutor}><strong>do</strong></span>
+          <span className={style.personalizado}><strong>Saber</strong></span>
+          <h2 className={style.subtitle}><strong>Aprenda com praticidade!</strong></h2>
+          <button className={style.journey} onClick={handleChatClick}><strong> <BsSlack/>  &nbsp; Chat de aprendizagem </strong> </button>
+        </div>
+      }
+
       {showChat && <ChatGPT />}
-      {showChat && <button className={`${style.exit} ${style.icon}`} onClick={() => setShowChat(false)}> <AiOutlineCloseCircle /> Voltar ao Inicio </button>}
-    
+
+      {!showChat && !showQuiz &&
+        <div className={`${style.inicar} ${style.quiz}`}>
+          <button className={style.buttonquiz} onClick={handleQuizClick}><strong> <BsJoystick/>  &nbsp; Teste Seu Conhecimento </strong> </button>
+        </div>
+      }
+      
+      {showQuiz && <Quiz />}
     </div>
-    
   );
 }
